ed1d88c28e34a929adfd06b3f0a25473f5c59f16
Alex Rawlings <alexander.rawlings@helsinki.fi>
2023-05-18 10:28:24 +0300

    Update eccentricity plots to use new colour scheme

--- papers/paper-eccentricity/scripts/eccentricity_scatter.py
+++ papers/paper-eccentricity/scripts/eccentricity_scatter.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cm_functions as cmf
 import figure_config
+from figure_config import plotter
 
 
 #------------------------------------------------------------
@@ -18,15 +19,12 @@ ax.set_xlabel(r"$M_\bullet/m_\star$")
 ax.set_xscale("log")
 ax.set_ylabel(r"$\sigma_e$")
 ax.set_yscale("log")
-marker_kwargs = {"edgecolor":"k", "lw":0.5}
-cmapper, sm = cmf.plotting.create_normed_colours(5e-3, 0.2, cmap="BuPu", normalisation="LogNorm")
-markers = cmf.plotting.mplChars()
 
 
 # plot data
 for i, (d, lab) in enumerate(zip((data_e090, data_e099), (r"$e_0=0.90$", r"$e_0=0.99$"))):
-    sc = ax.scatter(d["mass_res"], d["sigma_e"], label=f"{lab}", c=cmapper(1-d["e_ini"]), marker=markers[0], zorder=10, **marker_kwargs)
-    ax.scatter(d["mass_res"], d["sigma_e_cut"], label=f"{lab} ($<{d['sigma_e_cut_threshold']:.1f}\\sigma$)", c=cmapper(1-d["e_ini"]), marker=markers[1], s=sc.get_sizes()*1.5, zorder=1, **marker_kwargs)
+    plotter.scatter(d["mass_res"], d["sigma_e"], ax=ax, label=f"{lab}", zorder=10)
+    plotter.scatter(d["mass_res"], d["sigma_e_cut"], ax=ax, label=f"{lab} ($<{d['sigma_e_cut_threshold']:.1f}\\sigma$)", zorder=1)
 
 # add the sqrt{resolution} scaling line
 mass_res_seq = np.geomspace(2e3, 5e4, 10)
@@ -37,7 +35,7 @@ cmf.plotting.nice_log10_scale(ax, "xy")
 ax.legend(fontsize="small")
 
 # save
-cmf.plotting.savefig(figure_config.fig_path("convergence.pdf"), force_ext=True)
+cmf.plotting.savefig(figure_config.fig_path("convergence.png"), force_ext=True)
 plt.close(fig)
 
 
@@ -46,7 +44,7 @@ plt.close(fig)
 #------------------------------------------------------------
 
 # read in data
-data_2M = cmf.utils.load_data("nasim_scatter_res-2.0e+04.pickle")
+data_2M = cmf.utils.load_data(figure_config.data_path("nasim_scatter_res-2.0e+04.pickle"))
 
 # set up the figure
 fig, ax = plt.subplots(1,1)
@@ -54,7 +52,7 @@ ax.set_xlabel(r"$e_0$")
 ax.set_ylabel(r"$\sigma_e$")
 ax.set_yscale("log")
 
-ax.scatter(data_2M["e_ini"], data_2M["sigma_e"], **marker_kwargs)
+plotter.scatter(data_2M["e_ini"], data_2M["sigma_e"], ax=ax)
 cmf.plotting.nice_log10_scale(ax, "y")
-cmf.plotting.savefig(figure_config.fig_path("eccentricities.pdf"), force_ext=True)
+cmf.plotting.savefig(figure_config.fig_path("eccentricities.png"), force_ext=True)
 plt.close(fig)
