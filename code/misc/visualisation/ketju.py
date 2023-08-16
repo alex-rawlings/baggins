f947388d27ecfe04a1aa8a2c820faa751ae9a09e
Alex Rawlings <alexander.rawlings@helsinki.fi>
2023-08-16 11:00:00 +0300

    Further parameter files to try and find a high eccentricity merger
    - some figure scripts for seminar presentation

--- code/misc/visualisation/ketju.py
+++ code/misc/visualisation/ketju.py
@@ -13,7 +13,7 @@ def bh_inset(ax, bounds, bhs, bh_num, stars):
     axins.set_xticks([])
     axins.set_yticks([])
     axins.set_facecolor((0.1, 0.1, 0.1))
-    delta = 4e-3
+    delta = 2e-3
     axins.set_xlim(bhs["pos"][bh_num, 0]-delta, bhs["pos"][bh_num, 0]+delta)
     axins.set_ylim(bhs["pos"][bh_num, 2]-delta, bhs["pos"][bh_num, 2]+delta)
     ax.indicate_inset_zoom(axins, edgecolor="w", linewidth=2)
@@ -24,14 +24,13 @@ def bh_inset(ax, bounds, bhs, bh_num, stars):
     # add the bh
     axins.scatter(bhs["pos"][:, 0], bhs["pos"][:, 2], color="k", edgecolor="w", linewidth=0.5, s=50)
     # add the Ketju region
-    circle = Circle((bhs["pos"][bh_num, 0], bhs["pos"][bh_num, 2]), 2e-3, ec="w", fc="none", ls="--")
+    circle = Circle((bhs["pos"][bh_num, 0], bhs["pos"][bh_num, 2]), delta/2, ec="w", fc="none", ls="--")
     axins.add_patch(circle)
 
 
 
 
-pidx = "009"
-snapfile = f"/scratch/pjohanss/arawling/collisionless_merger/mergers/A-C-3.0-0.05/perturbations/{pidx}/output/AC_perturb_{pidx}_001.hdf5"
+snapfile = f"/scratch/pjohanss/arawling/collisionless_merger/mergers/eccentricity_study/e-090/4M/D_4M_a-D_4M_b-3.720-0.279/output/snap_003.hdf5"
 #snapfile = "/scratch/pjohanss/arawling/collisionless_merger/mergers/A-C-3.0-0.05/output/A-C-3.0-0.05_030.hdf5"
 
 snap = pygad.Snapshot(snapfile, physical=True)
@@ -39,7 +38,7 @@ snap = pygad.Snapshot(snapfile, physical=True)
 fig, ax = plt.subplots(1,1)
 ax.set_facecolor("k")
 ax.set_aspect("equal")
-plot_kwargs = {"cbartitle":r"log$_{10}\Sigma$ [M$_\odot \mathrm{kpc}^{-2}$]", "Npx":800, "qty":"mass", "fontsize":10, "xaxis":0, "yaxis":2}
+plot_kwargs = {"cbartitle":r"$\log_{10}\left( \Sigma / (\mathrm{M}_\odot \mathrm{kpc}^{-2}) \right)$", "Npx":800, "qty":"mass", "fontsize":10, "xaxis":0, "yaxis":2, "cmap":"cividis"}
 ball_mask = pygad.BallMask(30, snap.bh["pos"][0,:])
 pygad.plotting.image(snap.stars[ball_mask], ax=ax, **plot_kwargs)
 
@@ -48,5 +47,5 @@ pygad.plotting.image(snap.stars[ball_mask], ax=ax, **plot_kwargs)
 bh_inset(ax, [0.05, 0.5, 0.3, 0.3], snap.bh, 0, snap.stars)
 bh_inset(ax, [0.65, 0.02, 0.3, 0.3], snap.bh, 1, snap.stars)
 
-cmf.plotting.savefig(os.path.join(cmf.FIGDIR, "ketju.png"), fig=fig)
+cmf.plotting.savefig(os.path.join(cmf.FIGDIR, "ketju.pdf"), fig=fig, force_ext=True)
 plt.show()
