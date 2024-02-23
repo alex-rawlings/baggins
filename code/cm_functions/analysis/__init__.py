d5963ea99812d459d160f82ce36080bd6bcf227a
Alex Rawlings <alexander.rawlings@helsinki.fi>
2024-02-23 16:58:02 +0100

    tweaks reformatting to meet pep8 standards

--- code/cm_functions/analysis/__init__.py
+++ code/cm_functions/analysis/__init__.py
@@ -5,25 +5,25 @@ from .orbit import *  # noqa
 from .voronoi import *  # noqa
 
 from .analysis_classes import (
-    BHBinaryData, # noqa
-    BHBinary, # noqa
-    ChildSimData, # noqa
-    ChildSim, # noqa
-    HierarchicalModel_1D, # noqa
-    HierarchicalModel_2D, # noqa
-    HMQuantitiesSingleData, # noqa
-    HMQuantitiesSingle, # noqa
-    HMQuantitiesBinaryData, # noqa
-    HMQuantitiesBinary, # noqa
-    GrahamModelSimple, # noqa
-    GrahamModelHierarchy, # noqa
-    GrahamModelKick, # noqa
-    KeplerModelSimple, # noqa
-    KeplerModelHierarchy, # noqa
-    QuinlanModelSimple, # noqa
-    QuinlanModelHierarchy, # noqa
-    PQModelSimple, # noqa
-    PQModelHierarchy, # noqa
-    DeflectionAngleGP, # noqa
-    CoreKick, # noqa
+    BHBinaryData,  # noqa
+    BHBinary,  # noqa
+    ChildSimData,  # noqa
+    ChildSim,  # noqa
+    HierarchicalModel_1D,  # noqa
+    HierarchicalModel_2D,  # noqa
+    HMQuantitiesSingleData,  # noqa
+    HMQuantitiesSingle,  # noqa
+    HMQuantitiesBinaryData,  # noqa
+    HMQuantitiesBinary,  # noqa
+    GrahamModelSimple,  # noqa
+    GrahamModelHierarchy,  # noqa
+    GrahamModelKick,  # noqa
+    KeplerModelSimple,  # noqa
+    KeplerModelHierarchy,  # noqa
+    QuinlanModelSimple,  # noqa
+    QuinlanModelHierarchy,  # noqa
+    PQModelSimple,  # noqa
+    PQModelHierarchy,  # noqa
+    DeflectionAngleGP,  # noqa
+    CoreKick,  # noqa
 )
