00318e5a2ac6083e9273c5cc307142f956d491e7
Alex Rawlings <alexander.rawlings@helsinki.fi>
2024-03-01 17:59:44 +0200

    Fix escape velocity normalisation

--- code/cm_functions/env_config.py
+++ code/cm_functions/env_config.py
@@ -94,3 +94,18 @@ else:
 
 # make sure we are using a high enough version of python
 assert sys.version_info >= (3, 8, 6), "Required python version >= 3.8.6"
+
+# purely for fun # TODO print this to screen if verbose?
+"""
+888888b.         d8888  .d8888b.   .d8888b.  d8b           .d8888b.  
+888  "88b       d88888 d88P  Y88b d88P  Y88b Y8P          d88P  Y88b 
+888  .88P      d88P888 888    888 888    888              Y88b.      
+8888888K.     d88P 888 888        888        888 88888b.   "Y888b.   
+888  "Y88b   d88P  888 888  88888 888  88888 888 888 "88b     "Y88b. 
+888    888  d88P   888 888    888 888    888 888 888  888       "888 
+888   d88P d8888888888 Y88b  d88P Y88b  d88P 888 888  888 Y88b  d88P 
+8888888P" d88P     888  "Y8888P88  "Y8888P88 888 888  888  "Y8888P"  
+                                                                     
+                                                                     
+                                                                     
+"""
