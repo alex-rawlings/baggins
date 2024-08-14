2b10ebfb68ca8d8baffaf7cb845ca8d8c2b03d1f
Alex Rawlings <alexander.rawlings@helsinki.fi>
2024-08-14 10:46:15 +0300

    Formatting changes to velocity anisotropy script

--- papers/paper-corekick/scripts/v_anisotropy.py
+++ papers/paper-corekick/scripts/v_anisotropy.py
@@ -1,4 +1,4 @@
-## For calculating the (velocity) anisotropy parameter beta as a funtion of radius
+# For calculating the (velocity) anisotropy parameter beta as a funtion of radius
 import numpy as np
 from numpy import nanmedian as med
 import matplotlib.pyplot as plt
@@ -11,30 +11,30 @@ import figure_config
 start_time = time.time()
 
 
-## Options
-read_betas = 1  ## read in previously computed beta profiles
+# Options
+read_betas = 1  # read in previously computed beta profiles
 show_fig = 1
-save_fig = 1  ## save output figure
-save_betas = 1  ## save beta profiles to output data file (only if read_betas == 0)
+save_fig = 1  # save output figure
+save_betas = 1  # save beta profiles to output data file (only if read_betas == 0)
 sample_size = (
-    10000  ## number of core radii sampled per simulation (only if read_file == 0)
+    10000  # number of core radii sampled per simulation (only if read_file == 0)
 )
 x_axis = (
-    "v"  ## whether to plot betas as a function of kick velocity ("v") or radius ("r")
+    "v"  # whether to plot betas as a function of kick velocity ("v") or radius ("r")
 )
 
-input_file = "beta_profiles_vkick_N10000.pickle"  ## file containing input beta profiles if read_file == 1
+input_file = "beta_profiles_vkick_N10000.pickle"  # file containing input beta profiles if read_file == 1
 output_file = (
-    "beta_profiles_vkick.pdf"  ## base filename for the output figure and data file
+    "beta_profiles_vkick.pdf"  # base filename for the output figure and data file
 )
 
-# fig_path = this_dir + "/../figures/" ## directory for output figure
-data_path = "/scratch/pjohanss/arawling/collisionless_merger/mergers/processed_data/core-paper-data/"  ## directory for input core radii, input beta profiles and output data file
-snap_path = "/scratch/pjohanss/arawling/collisionless_merger/mergers/core-study/vary_vkick/"  ## directory for input snapshots
-core_file = "core-kick.pickle"  ## file containing core radii values
+# fig_path = this_dir + "/../figures/" # directory for output figure
+data_path = "/scratch/pjohanss/arawling/collisionless_merger/mergers/processed_data/core-paper-data/"  # directory for input core radii, input beta profiles and output data file
+snap_path = "/scratch/pjohanss/arawling/collisionless_merger/mergers/core-study/vary_vkick/"  # directory for input snapshots
+core_file = "core-kick.pickle"  # file containing core radii values
 
 
-## Kick velocities and corresponding snapshot numbers to use
+# Kick velocities and corresponding snapshot numbers to use
 vels = [
     "0000",
     "0060",
@@ -58,17 +58,17 @@ vels = [
 snaps = [2, 4, 4, 9, 9, 13, 31, 50, 70, 60, 73, 74, 100, 143, 193, 199, 240, 275]
 
 
-## Compute polar angle given xyz position
+# Compute polar angle given xyz position
 def theta(x, y, z):
     return np.arccos(z / np.sqrt(x**2 + y**2 + z**2))
 
 
-## Compute azimuthal angle given xy position
+# Compute azimuthal angle given xy position
 def phi(x, y):
     return np.sign(y) * np.arccos(x / np.sqrt(x**2 + y**2))
 
 
-## Compute spherical velocities (v_r, v_theta, v_phi) for a given (sub)snapshot
+# Compute spherical velocities (v_r, v_theta, v_phi) for a given (sub)snapshot
 def spherical_velocity(snapshot):
     x, y, z = (
         snapshot["pos"][:, 0],
@@ -80,31 +80,31 @@ def spherical_velocity(snapshot):
         snapshot["vel"][:, 1],
         snapshot["vel"][:, 2],
     )
-    t, p = theta(x, y, z), phi(x, y)  ## theta, phi arrays
+    t, p = theta(x, y, z), phi(x, y)  # theta, phi arrays
     v_r = (
         np.sin(t) * np.cos(p) * vx + np.sin(t) * np.sin(p) * vy + np.cos(t) * vz
-    )  ## v_r
+    )  # v_r
     v_t = (
         np.cos(t) * np.cos(p) * vx + np.cos(t) * np.sin(p) * vy - np.sin(t) * vz
-    )  ## v_theta
-    v_p = -np.sin(p) * vx + np.cos(p) * vy  ## v_phi
+    )  # v_theta
+    v_p = -np.sin(p) * vx + np.cos(p) * vy  # v_phi
 
     return v_r, v_t, v_p
 
 
-## Compute anisotropy parameter beta for a given (sub)snapshot
+# Compute anisotropy parameter beta for a given (sub)snapshot
 def beta(snapshot):
     v_r, v_t, v_p = spherical_velocity(snapshot)
     return 1 - (np.std(v_t) ** 2 + np.std(v_p) ** 2) / (2 * np.std(v_r) ** 2 + 1e-16)
 
 
-## Compute anisotropy parameter beta profile for a given (sub)snapshot.
-## Profile is computed in 2 bins with respect to r_split in [kpc], i.e. r <= r_split and r > r_split.
+# Compute anisotropy parameter beta profile for a given (sub)snapshot.
+# Profile is computed in 2 bins with respect to r_split in [kpc], i.e. r <= r_split and r > r_split.
 def beta_profile(snapshot, r_split):
     b_arr = np.full(2, np.nan)
     bin_counts = np.full(2, np.nan)
 
-    ## radial masks for r <= r_split and r > r_split
+    # radial masks for r <= r_split and r > r_split
     m1, m2 = pygad.ExprMask("r <= " + str(r_split)), pygad.ExprMask(
         "r > " + str(r_split)
     )
@@ -112,10 +112,10 @@ def beta_profile(snapshot, r_split):
     for i in range(2):
         b_arr[i] = beta(snapshot[m[i]])
         bin_counts[i] = len(snapshot[m[i]]["r"])
-    ## Compute mean radii for the bins
+    # Compute mean radii for the bins
     r_arr = np.array([np.mean(snapshot[m1]["r"]), np.mean(snapshot[m2]["r"])])
 
-    ## Return bin radii and corresponding beta values and bin counts
+    # Return bin radii and corresponding beta values and bin counts
     return (
         r_arr,
         b_arr,
@@ -123,16 +123,16 @@ def beta_profile(snapshot, r_split):
     )
 
 
-## Create random number generator
+# Create random number generator
 rgen = np.random.default_rng(seed=88965)
 
 
-## Extract input core radii
+# Extract input core radii
 with open(data_path + core_file, "rb") as f:
     core_radii = pickle.load(f)["rb"]
 
 
-## Compute new beta profiles...
+# Compute new beta profiles...
 if read_betas == 0:
     print("Computing beta profiles...", flush=True)
     extracted_data = {
@@ -159,19 +159,19 @@ if read_betas == 0:
             physical=True,
         )
 
-        ## Find center using shrinking sphere on the stars
+        # Find center using shrinking sphere on the stars
         center = pygad.analysis.shrinking_sphere(
             snap.stars, center=pygad.analysis.center_of_mass(snap.stars), R=30
         )
 
-        ## Shift coordinates of all particles so that the snap is centered on 
+        # Shift coordinates of all particles so that the snap is centered on
         # the shrinking sphere result
         pygad.Translation(-center).apply(snap)
 
         this_core_radii = core_radii[vels[i]].flatten()
         core_sample = rgen.choice(
             this_core_radii, size=sample_size, replace=True
-        )  ## select core radius sample with rgen
+        )  # select core radius sample with rgen
         for j in range(len(core_sample)):
             radii, betas, bincount = beta_profile(snap.stars, r_split=core_sample[j])
             r_in, r_out, b_in, b_out = radii[0], radii[-1], betas[0], betas[-1]
@@ -185,13 +185,13 @@ if read_betas == 0:
         pygad.gc_full_collect()
         print(time.time() - step_time, "s", flush=True)
 
-    ## Save beta profiles
+    # Save beta profiles
     if save_betas == 1:
         print("Saving beta profiles...", flush=True)
         print(data_path + output_file + ".pickle", flush=True)
         pickle.dump(extracted_data, open(data_path + output_file + ".pickle", "wb"))
 
-## ...or read in previously computed beta profiles
+# ...or read in previously computed beta profiles
 elif read_betas == 1:
     print("Reading beta profiles...", flush=True)
     print(data_path + input_file, flush=True)
@@ -202,13 +202,13 @@ else:
 
 
 if show_fig == 1 or save_fig == 1:
-    ## Initialize figure
+    # Initialize figure
     fig, ax = plt.subplots()
     ax.axhline(
         y=0, xmin=0, xmax=1, linestyle=":", color="k", zorder=0, lw=1
-    )  ## zero level on the background
+    )  # zero level on the background
 
-    ## Plot the profiles
+    # Plot the profiles
     cols = figure_config.color_cycle_shuffled.by_key()["color"][:2]
     for i in range(len(vels)):
         r_in, r_out = extracted_data[vels[i]]["r_in"], extracted_data[vels[i]]["r_out"]
@@ -285,7 +285,7 @@ if show_fig == 1 or save_fig == 1:
     )
 
 
-## Save and show figure
+# Save and show figure
 if save_fig == 1:
     print("Saving figure...", flush=True)
     plt.savefig(figure_config.fig_path(output_file))
