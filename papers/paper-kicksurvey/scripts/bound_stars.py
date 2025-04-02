d473aebc265afa2b825366aa2fce451ce0728aa5
Alex Rawlings <alexander.rawlings@helsinki.fi>
2025-04-02 16:12:42 +0300

    Update bound figure

--- papers/paper-kicksurvey/scripts/bound_stars.py
+++ papers/paper-kicksurvey/scripts/bound_stars.py
@@ -4,8 +4,6 @@ from datetime import datetime
 from tqdm import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.lines import Line2D
-from matplotlib.ticker import FixedLocator
 import baggins as bgs
 import pygad
 import figure_config
@@ -140,149 +138,125 @@ if args.extract:
 data_files = bgs.utils.get_files_in_dir(main_data_dir, ".pickle")
 
 # set up the figure
-fig, ax = plt.subplots(1, 2)
-fig.set_figwidth(2 * fig.get_figwidth())
-vkcols = figure_config.VkickColourMap()
-ax[0].set_xlabel(r"$t/t_{\mathrm{cross},\bullet}$")
-ax[0].set_ylabel(r"$r/\mathrm{kpc}$")
-ax[1].set_xlabel(r"$v_\mathrm{kick}/\mathrm{km\,s}^{-1}$")
-ax[1].set_ylabel(r"$M_\mathrm{bound}/\mathrm{M}_\odot$")
-linthresh = 3
-ax[0].set_yscale("symlog", linthresh=linthresh)
-
-bp = None
+fig, ax = plt.subplots(1, 1)
+fig.set_figwidth(1.2 * fig.get_figwidth())
+ax.set_xlabel(r"$v_\mathrm{kick}/\mathrm{km\,s}^{-1}$")
+ax.set_ylabel(r"$M_\mathrm{bound}/\mathrm{M}_\odot$")
 
 
-def marker_size_scale(x):
-    log_x = np.log10(x)
-    x0 = (np.log10(5e5) + np.log10(5e9)) / 2  # centre in log10 space
-    k = 2
-    return 30 / (1 + np.exp(-k * (log_x - x0))) + 2
+def data_grabber():
+    """
+    Generator to get the data to plot
+
+    Yields
+    ------
+    return_dict : dict
+        plotting data
+    """
+    for i, df in enumerate(data_files):
+        data = bgs.utils.load_data(df)
+        # XXX temp fix
+        vk = float(
+            os.path.splitext(os.path.basename(df))[0]
+            .replace("kick-vel-", "")
+            .replace("-bound", "")
+        )
+        if i == 0:
+            mstar = data["other"]["mstar"]
+            m_bh = 2 * data["other"]["mbh"]  # BH yet to merge
+        # need to find first pericentre
+        return_dict = {
+            "vk": vk,
+            "bound_a": np.nan,
+            "bound_p": np.nan,
+            "r_a": np.nan,
+            "r_p": np.nan,
+            "m_bh": m_bh,
+        }
+        try:
+            apo_idx = np.nanargmax(data["r"])
+            peri_idx = np.nanargmax(np.diff(data["r"][apo_idx:]) > 0) + apo_idx + 1
+            return_dict["bound_a"] = mstar * data["Nbound"][apo_idx]
+            return_dict["bound_p"] = mstar * data["Nbound"][peri_idx]
+            return_dict["r_a"] = data["r"][apo_idx]
+            return_dict["r_p"] = data["r"][peri_idx]
+        except ValueError as err:
+            SL.warning(f"Skipping: {vk}")
+            SL.debug(err)
+        yield return_dict
 
 
-for i, df in enumerate(data_files):
-    data = bgs.utils.load_data(df)
-    # XXX temp fix
-    vk = float(
-        os.path.splitext(os.path.basename(df))[0]
-        .replace("kick-vel-", "")
-        .replace("-bound", "")
-    )
-    if i == 0:
-        mstar = data["other"]["mstar"]
-        m_bh = 2 * data["other"]["mbh"]  # BH yet to merge
-    # need to find first pericentre
-    try:
-        apo_idx = np.nanargmax(data["r"])
-        peri_idx = np.nanargmax(np.diff(data["r"][apo_idx:]) > 0) + apo_idx + 1
-    except ValueError as err:
-        SL.warning(f"Skipping: {vk}")
-        SL.debug(err)
+grab_data = data_grabber()
+max_r = np.nanmax(list(d["r_a"] for d in grab_data))
+SL.debug(f"Maximum radius is {max_r}")
+r_col_mapper, sm = bgs.plotting.create_normed_colours(
+    1e-3, max_r, cmap="rocket", norm="LogNorm"
+)
+
+grab_data = data_grabber()
+reff_vel = None
+for d in grab_data:
+    if d["vk"] < 1 or d["vk"] > args.maxvel:
+        m_bh = 2 * d["m_bh"]
         continue
-
-    # rescale time to 2 * crossing time: tc = R/v
-    t = (data["t"][:peri_idx] - data["t"][~np.isnan(data["t"])][0]) / (
-        data["t"][peri_idx] - data["t"][~np.isnan(data["t"])][0]
+    ax.semilogy([d["vk"]] * 2, [d["bound_a"], d["bound_p"]], lw=0.5, ls="-", c="k")
+    ax.semilogy(
+        d["vk"],
+        d["bound_p"],
+        marker="s",
+        ls="",
+        mew="0.5",
+        mec="k",
+        c=r_col_mapper(d["r_p"]),
     )
-    if vk <= args.maxvel:
-        (lp,) = ax[0].plot(
-            t, data["r"][:peri_idx], c=vkcols.get_colour(vk), ls="-", zorder=0.3, lw=1
-        )
-        # create marker sizes
-        msizes = marker_size_scale(mstar * data["Nbound"][:peri_idx])
-        ax[0].scatter(
-            t,
-            data["r"][:peri_idx],
-            c=lp.get_color(),
-            s=msizes,
-            **figure_config.marker_kwargs,
-        )
-    if bp is None:
-        (bp,) = ax[1].semilogy(
-            vk, mstar * data["Nbound"][apo_idx], marker="o", ls="", mew="0.5", mec="k"
-        )
-    else:
-        ax[1].semilogy(
-            vk,
-            mstar * data["Nbound"][apo_idx],
-            marker="o",
-            ls="",
-            c=bp.get_color(),
-            mew="0.5",
-            mec="k",
-        )
-vkcols.make_cbar(ax[0])
-
-# create the top legend for marker sizes
-legend_masses = 10 ** np.arange(7, 9.1, 1)
-# Create custom legend handles
-legend_handles = [
-    Line2D(
-        [],
-        [],
+    ax.semilogy(
+        d["vk"],
+        d["bound_a"],
         marker="o",
-        color="w",
-        label=f"{np.log10(lm):.1f}",
-        markerfacecolor="dimgray",
-        markersize=np.sqrt(marker_size_scale(lm)),
+        ls="",
+        mew="0.5",
+        mec="k",
+        c=r_col_mapper(d["r_a"]),
     )
-    for lm in legend_masses
-]
-# Add the legend above the plot
-ax[0].legend(
-    handles=legend_handles,
-    title=r"$\log_{10}(M_\mathrm{bound}/\mathrm{M}_\odot)$",
-    loc="lower center",
-    bbox_to_anchor=(0.5, 1.02),
-    ncol=5,
-    frameon=True,
-)
+    if d["r_a"] > eff_radius and reff_vel is None:
+        reff_vel = d["vk"]
+plt.colorbar(sm, ax=ax, label=r"$r/\mathrm{kpc}$", location="top")
 
-# set regions to show core radius and effective radius
-ylim = ax[0].get_ylim()
-ax[0].axhspan(ylim[0], core_radius, fc="dimgray", alpha=0.6, zorder=0.1)
-ax[0].text(0.3, 0.5 * core_radius, r"$r<r_{\mathrm{b},0}$")
-ax[0].axhspan(ylim[0], eff_radius, fc="gray", alpha=0.4, zorder=0.1)
-ax[0].text(0.3, 0.25 * eff_radius, r"$r<R_\mathrm{e}$")
-ax[0].set_ylim(ylim)
-
-# show core dispersion
-xlim = ax[1].get_xlim()
-ax[1].axvspan(xlim[0], core_dispersion, fc="dimgray", alpha=0.6, zorder=1)
-ax[1].text(
-    core_dispersion * 0.3,
-    ax[1].get_ylim()[1] * 1e-3,
-    r"$v_\mathrm{kick}< \sigma_{\star,0}$",
-    rotation="vertical",
+ax.scatter(
+    [], [], marker="o", c="gray", lw="0.5", ec="k", label=r"$\mathrm{apocentre}$"
 )
-ax[1].set_xlim(xlim)
+ax.scatter(
+    [], [], marker="s", c="gray", lw="0.5", ec="k", label=r"$\mathrm{pericentre}$"
+)
+ax.legend()
 
 # set dual y axis on second plot
-ax[1].tick_params(axis="y", which="both", right=False)
-axr = ax[1].secondary_yaxis("right", functions=(lambda x: x / m_bh, lambda x: x * m_bh))
+ax.tick_params(axis="y", which="both", right=False)
+axr = ax.secondary_yaxis("right", functions=(lambda x: x / m_bh, lambda x: x * m_bh))
 axr.set_ylabel(r"$M_\mathrm{bound}/M_\bullet$")
 
+# show core dispersion
+xlim = ax.get_xlim()
+ax.axvspan(
+    xlim[0], core_dispersion, zorder=1, hatch="//", fc="none", ec="dimgray", lw=1
+)
+ax.text(
+    0.1,
+    0.5,
+    r"$v_\mathrm{kick}< \sigma_{\star,0}$",
+    rotation="vertical",
+    transform=ax.transAxes,
+    va="center",
+    bbox={"fc": "w", "ec": "none"},
+)
 
-# Generate manual minor tick positions (log space, skipping linear region)
-def log_minor_ticks(linthresh, max_exp=5):
-    ticks = []
-    # Positive side
-    for exp in range(1, max_exp + 1):
-        for sub in range(2, 10):
-            val = sub * 10 ** (exp - 1)
-            if val > linthresh:
-                ticks.append(val)
-    # Negative side (mirror)
-    ticks += [-t for t in ticks]
-    return sorted(ticks)
-
-
-minor_ticks = log_minor_ticks(linthresh=np.floor(np.log10(linthresh)))
-
-# Set manual minor ticks
-ax[0].yaxis.set_minor_locator(FixedLocator(minor_ticks))
-
-# Customize tick appearance
-ax[0].tick_params(axis="y", which="minor", color="k")
+# show were r_apo > Re
+ax.axvspan(reff_vel, xlim[1], alpha=0.6, zorder=1, fc="gray")
+ax.text(
+    1.1 * reff_vel,
+    1e7,
+    r"$r_\mathrm{apo} > R_\mathrm{e}$",
+)
+ax.set_xlim(xlim)
 
 bgs.plotting.savefig(figure_config.fig_path("bound.pdf"), force_ext=True)
